2a798bf650770a2cda23c232e646eb4136121a1b
Rufus Buschart <rufus.buschart@siemens.com>
2019-10-01 13:00:58 +0200

    Update to 0.73

--- puTTY CAC/tools/chocolateyinstall.ps1
+++ puTTY CAC/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.72/binaries/puttycac-0.72-installer.msi'
-$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.72/binaries/puttycac-64bit-0.72-installer.msi'
+$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.73/binaries/puttycac-0.73-installer.msi'
+$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.73/binaries/puttycac-64bit-0.73-installer.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName  = 'puTTY CAC*'
 
-  checksum      = '731FE6CAED39416D9CD738FE70C1C43C0099B20BF0E5F3A229C8131E21AB3F0A'
+  checksum      = 'D442E87A100F2D4C4056B368B9A00F763ED4351712EEB805739C3DF143B79E9E'
   checksumType  = 'sha256'
-  checksum64    = '5799EE7200FDB4F1D81ECB3315089576168903569A1AA8DD283D984BBD1B00BC'
+  checksum64    = 'BB534F888DEA7150ABDA5249C393D41E64F57B9396BCBB44AABF85A06BAC412A'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
