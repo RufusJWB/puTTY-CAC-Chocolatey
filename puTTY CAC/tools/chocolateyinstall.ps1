e86ac106dbfa0ed8e3a5ec27604490d176d58ca8
Rufus Buschart <rufus.buschart@siemens.com>
2021-07-05 10:25:00 +0200

    Updated to 0.75u1

--- puTTY CAC/tools/chocolateyinstall.ps1
+++ puTTY CAC/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.74/binaries/puttycac-0.74-installer.msi'
-$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.74/binaries/puttycac-64bit-0.74-installer.msi'
+$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.75u1/binaries/puttycac-0.75u1-installer.msi'
+$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.75u1/binaries/puttycac-64bit-0.75u1-installer.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName  = 'puTTY CAC*'
 
-  checksum      = 'E47526DB93933B961C0D5DA59112AED28504B77C47DEBD0921252DAF0655BEA2'
+  checksum      = '397809F0C81F8A1E8D22D658A777DD491310B5831D286C1A985712C880F3710F'
   checksumType  = 'sha256'
-  checksum64    = '2AEDF0FC5B97AC524F2BC5B150C4D4B963D588C65F7BDE92D0F4B55D895700E7'
+  checksum64    = '80FEFD3B7CD3A449C6D2B04A116CBAAEBD09C1F150A128C8249140DB394349CC'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
